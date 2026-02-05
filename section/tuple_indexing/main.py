60b605fa5ea0b494448fde7632878e6e0751b320
gedrn <>
2026-02-05 18:55:35 +0000

    Tuple Indexing

---
+++ section/tuple_indexing/main.py
@@ -0,0 +1,10 @@
+space_movies = ('2001: A Space Odyssey', 'Interstellar', 'Star Wars: Episode IV - A New Hope', 'Gravity', 'The Martian')
+
+# Write your code here
+first_movie = '2001: A Space Odyssey', 'Interstellar', 'Star Wars: Episode IV - A New Hope', 'Gravity', 'The Martian'[0]
+last_movie = space_movies[-1]
+
+favorite_movies = ___
+
+# Testing 
+print("Favorite movies about space:", favorite_movies)
