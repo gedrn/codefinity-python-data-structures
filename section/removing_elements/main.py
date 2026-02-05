1b4beda1e4d2bc483d70a36412a3b7bdd3a5b7ce
gedrn <>
2026-02-05 18:23:36 +0000

    Removing Elements

---
+++ section/removing_elements/main.py
@@ -0,0 +1,8 @@
+travel_wishlist = ["Paris", "Oslo", "Kyoto", "Sydney"]
+
+# Deleting elements
+travel_wishlist.remove("Oslo")
+travel_wishlist.remove("Sydney")
+
+# Testing
+print('Updated list:', travel_wishlist)
